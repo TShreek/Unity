a56e8ffc4c8a77cef09e1e00461970d2be705f8d
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-02-01 00:43:13 +0530

    add 3 fences

--- Unity 3d/Hell runner/Assets/Scripts/Chunk.cs
+++ Unity 3d/Hell runner/Assets/Scripts/Chunk.cs
@@ -1,21 +1,31 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Chunk : MonoBehaviour
 {
     [SerializeField] private GameObject fencePrefab;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private int numOfFences = 3;
+    private float toAdd = 50f / 3f; // Ensure float division
+    [SerializeField] private float[] lanes = { -1.36f, 2.21f, 5.31f };
+
     void Start()
     {
-        
+        spawnFence();
     }
 
-    void spawnFence()
+    private void spawnFence()
     {
-        
+        for (int i = 0; i < numOfFences; i++)
+        {
+            InstantiateFence(i * toAdd);
+        }
     }
-    // Update is called once per frame
-    void Update()
+
+    private void InstantiateFence(float z)
     {
-        
+        int FenceIndex = Random.Range(0, lanes.Length); // Get a valid lane index
+        float x = lanes[FenceIndex]; // Pick a lane position
+        Vector3 spawnPosition = new Vector3(transform.position.x + x, transform.position.y - 1.0f, transform.position.z + z);
+        Instantiate(fencePrefab, spawnPosition, Quaternion.identity,this.transform); 
     }
 }
