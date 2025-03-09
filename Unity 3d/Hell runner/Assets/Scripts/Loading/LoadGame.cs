73c226bc159eb1da137cc7d533f24c26dd8fb0bc
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-03-09 17:16:54 +0530

    add button

--- Unity 3d/Hell runner/Assets/Scripts/Loading/LoadGame.cs
+++ Unity 3d/Hell runner/Assets/Scripts/Loading/LoadGame.cs
@@ -1,10 +1,16 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadGame : MonoBehaviour
 {
+    IEnumerator loadScene(string sceneName)
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        SceneManager.LoadScene(sceneName);
+    }
     public void loadNextScene()
     {
-        
+        StartCoroutine(loadScene("Main Scene"));
     }
 }
