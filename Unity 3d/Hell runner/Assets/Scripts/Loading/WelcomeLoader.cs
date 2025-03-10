f69ad8f20a383e5a43c9e32ff487257a0dd58553
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-03-10 21:53:52 +0530

    add few obstacles

--- Unity 3d/Hell runner/Assets/Scripts/Loading/WelcomeLoader.cs
+++ Unity 3d/Hell runner/Assets/Scripts/Loading/WelcomeLoader.cs
@@ -1,12 +1,18 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class WelcomeLoader : MonoBehaviour
 {
-    IEnumerator loadScene(string sceneName)
+    IEnumerator loadScene()
     {
         yield return new WaitForSeconds(5f);
         SceneManager.LoadScene("Main Menu");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(loadScene());
     }
 }
